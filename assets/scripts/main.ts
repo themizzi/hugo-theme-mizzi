12f8c9d6e309c0e404942822712f40471364ca93
Joe Mizzi <themizzi@me.com>
2025-10-31 18:09:00 +0000

    feat(layouts): remove theme toggle

--- assets/scripts/main.ts
+++ assets/scripts/main.ts
@@ -1,28 +1,3 @@
-import { ThemeToggle } from './theme-toggle';
-
-// Initialize the ThemeToggle class with the necessary elements
-document.addEventListener('DOMContentLoaded', () => {
-  const themeColorMeta = document.getElementById('theme-color-meta') as HTMLElement;
-  const themeIconSystem = document.getElementById('theme-system') as HTMLElement;
-  const themeIconLight = document.getElementById('theme-light') as HTMLElement;
-  const themeIconDark = document.getElementById('theme-dark') as HTMLElement;
-
-  if (themeColorMeta && themeIconSystem && themeIconLight && themeIconDark) {
-    new ThemeToggle(
-      themeColorMeta,
-      [
-        themeIconSystem,
-        themeIconLight,
-        themeIconDark,
-      ],
-      document.documentElement,
-    );
-  }
-  else {
-    console.error('One or more theme elements are missing');
-  }
-});
-
 document.addEventListener('DOMContentLoaded', function () {
   // show all elements with .javascript-enable class
   document.querySelectorAll('.javascript-enable').forEach((element) => {
